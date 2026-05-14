a33daebfe2e3ba5337719c4b1ed5d3ef9657a64e
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-14 19:00:34 -0400

    refactor: improve entity annotations

--- src/main/java/com/duoc/product/model/ProductModel.java
+++ src/main/java/com/duoc/product/model/ProductModel.java
@@ -2,11 +2,13 @@ package com.duoc.product.model;
 
 import java.math.BigDecimal;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,16 +24,22 @@ public class ProductModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nombre;
 
+    @Column(nullable = false)
     private String descripcion;
-
+    
+    @Column(nullable = false)
     private BigDecimal precio;
 
+    @Column(nullable = false)
     private Integer stock;
 
+    @Column(nullable = false)
     private String categoria;
 
+    @Column(nullable = false)
     private Boolean activo;
 
 }
