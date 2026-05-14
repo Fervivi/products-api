8dbca61c9dd7bb3826188297686738c62ad3e7a2
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-14 19:23:13 -0400

    feat: add ProducRequestDto

--- src/main/java/com/duoc/product/dto/request/ProductRequestDto.java
+++ src/main/java/com/duoc/product/dto/request/ProductRequestDto.java
@@ -5,18 +5,15 @@ import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductRequestDto {
-
 
     private String nombre;
     private String descripcion;
     private BigDecimal precio;
     private Integer stock;
     private String categoria;
-    
 
 }
