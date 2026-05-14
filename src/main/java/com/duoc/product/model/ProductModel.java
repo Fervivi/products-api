6d2439be0063effc8757d23619e30699633b7513
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-14 18:49:21 -0400

    refactor: rename invoice entity class

--- src/main/java/com/duoc/product/model/ProductModel.java
+++ src/main/java/com/duoc/product/model/ProductModel.java
@@ -16,7 +16,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Product {
+public class ProductModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
