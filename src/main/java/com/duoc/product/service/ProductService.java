0d40b7b1008843f3312b87d37970a7f6505bfe9b
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-14 19:28:13 -0400

    feat: add service class

---
+++ src/main/java/com/duoc/product/service/ProductService.java
@@ -0,0 +1,14 @@
+package com.duoc.product.service;
+
+import org.springframework.stereotype.Service;
+
+import com.duoc.product.repository.ProductRepository;
+
+
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+}
