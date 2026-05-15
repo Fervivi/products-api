2b520949c87cae46ea3a69847efd6c6aa54e8ccb
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-15 00:08:49 -0400

    feat: add createProduct and mapToResponse method to Service

--- src/main/java/com/duoc/product/service/ProductService.java
+++ src/main/java/com/duoc/product/service/ProductService.java
@@ -6,6 +6,9 @@
  */
 package com.duoc.product.service;
 
+import com.duoc.product.dto.request.ProductRequestDto;
+import com.duoc.product.dto.response.ProductResponseDto;
+import com.duoc.product.model.ProductModel;
 import com.duoc.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,4 +18,34 @@ import org.springframework.stereotype.Service;
 public class ProductService {
 
     private ProductRepository productRepository;
+
+    public ProductResponseDto mapToResponseDto(ProductModel product) {
+
+        ProductResponseDto response = new ProductResponseDto();
+
+        response.setId(product.getId());
+        response.setNombre(product.getNombre());
+        response.setPrecion(product.getPrecio());
+        response.setStock(product.getStock());
+        response.setCategoria(product.getCategoria());
+        response.setActvo(product.getActivo());
+
+        return response;
+    }
+
+    public ProductResponseDto createProduct(ProductRequestDto request) {
+
+        ProductModel product = new ProductModel();
+
+        product.setNombre(request.getNombre());
+        product.setDescripcion(request.getDescripcion());
+        product.setPrecio(request.getPrecio());
+        product.setStock(request.getStock());
+        product.setCategoria(request.getCategoria());
+        product.setActivo(true);
+
+        ProductModel savedProduct = productRepository.save(product);
+
+        return mapToResponseDto(savedProduct);
+    }
 }
