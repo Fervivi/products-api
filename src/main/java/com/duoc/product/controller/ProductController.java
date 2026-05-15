5d8d097159f3f5ec284b09a79cbfbbb37c915ac1
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-15 00:19:08 -0400

    feat: add get endpoint createProduct

--- src/main/java/com/duoc/product/controller/ProductController.java
+++ src/main/java/com/duoc/product/controller/ProductController.java
@@ -6,9 +6,27 @@
  */
 package com.duoc.product.controller;
 
+import com.duoc.product.dto.request.ProductRequestDto;
+import com.duoc.product.dto.response.ProductResponseDto;
+import com.duoc.product.service.ProductService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/product")
-public class ProductController {}
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductRequestDto request) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(request));
+    }
+}
