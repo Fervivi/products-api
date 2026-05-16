a94eafcea0744ac425dc476a99878f837edceca9
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-16 15:36:24 -0400

    feat: add SecurityConfig class

--- src/main/java/com/duoc/product/secutiry/JwtAuthenticationFilter.java
+++ src/main/java/com/duoc/product/secutiry/JwtAuthenticationFilter.java
@@ -11,8 +11,12 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
---
+++ src/main/java/com/duoc/product/secutiry/SecurityConfig.java
@@ -0,0 +1,59 @@
+package com.duoc.product.secutiry;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+
+            // Puede lanzar excepción
+            throws Exception {
+
+        // Retorna configuración final de seguridad
+        return http
+
+                // Desactiva protección CSRF
+                .csrf(csrf -> csrf.disable())
+
+                // Configura aplicación Stateless
+                .sessionManagement(session ->
+
+                // No crear sesiones HTTP
+                session.sessionCreationPolicy(
+                        SessionCreationPolicy.STATELESS))
+
+                // Configura reglas de autorización
+                .authorizeHttpRequests(auth -> auth
+
+                        // Permite acceso público
+                        .requestMatchers("/api/v1/public/**").permitAll()
+
+                        // Cualquier otro endpoint requiere autenticación
+                        .anyRequest().authenticated())
+
+                // Agrega JwtAuthenticationFilter antes del filtro estándar
+                .addFilterBefore(
+
+                        // Nuestro filtro JWT personalizado
+                        jwtAuthenticationFilter,
+
+                        // Antes del filtro default Spring
+                        UsernamePasswordAuthenticationFilter.class)
+
+                // Construye configuración final
+                .build();
+    }
+
+}
