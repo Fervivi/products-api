4fcec74a86d0987c2971dbf9b3c5a5e7c3509166
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-15 23:41:28 -0400

    feat: add PostMapping annotation

--- src/main/java/com/duoc/product/controller/ProductController.java
+++ src/main/java/com/duoc/product/controller/ProductController.java
@@ -14,6 +14,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +26,7 @@ public class ProductController {
 
     private final ProductService productService;
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductRequestDto request) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(request));
