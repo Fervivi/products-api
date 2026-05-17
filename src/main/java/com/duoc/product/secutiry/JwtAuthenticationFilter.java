f11ddf9cce946a6909a28d46094952c0e07e219d
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-17 12:56:29 -0400

    style: organize imports

--- src/main/java/com/duoc/product/secutiry/JwtAuthenticationFilter.java
+++ src/main/java/com/duoc/product/secutiry/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@
  */
 package com.duoc.product.secutiry;
 
+import com.auth0.jwt.algorithms.Algorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
