9ef7abc0c1340b2830f04f17277b125bd8f757fa
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-20 15:37:41 -0400

    (feat: add validation ProductRequestDto)

--- src/main/java/com/duoc/product/dto/request/ProductRequestDto.java
+++ src/main/java/com/duoc/product/dto/request/ProductRequestDto.java
@@ -7,6 +7,10 @@
 package com.duoc.product.dto.request;
 
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,9 +20,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ProductRequestDto {
 
+    @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
+    
+    @NotBlank(message = "La descripción es obligatoria")
     private String descripcion;
+
+    @NotNull(message = "El precio es obligatorio")
+    @Positive(message = "El precio debe ser mayor a 0")
     private BigDecimal precio;
+
+    @NotNull(message = "El stock es obligatorio")
+    @PositiveOrZero(message = "El stock no puede ser negativo")
     private Integer stock;
+
+    @NotBlank(message = "La categoría es obligatoria")
     private String categoria;
 }
