9c4e517e43d896e570ee73650a056aaf48237f50
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-20 16:00:32 -0400

    chore: reorder imports and add valid

--- src/main/java/com/duoc/product/controller/ProductController.java
+++ src/main/java/com/duoc/product/controller/ProductController.java
@@ -10,9 +10,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,7 @@ import com.duoc.product.dto.request.ProductRequestDto;
 import com.duoc.product.dto.response.ProductResponseDto;
 import com.duoc.product.service.ProductService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -52,7 +55,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponseDto> createProduct(@RequestBody ProductRequestDto request) {
+    public ResponseEntity<ProductResponseDto> createProduct(@Valid@RequestBody ProductRequestDto request) {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.createProduct(request));
     }
@@ -70,11 +73,5 @@ public class ProductController {
         return ResponseEntity.ok(productService.deactivateProduct(id));
     }
 
-    @PutMapping("/{id}")
-public ResponseEntity<ProductResponseDto> updateProduct(
-        @PathVariable Long id,
-        @Valid @RequestBody ProductRequestDto request) {
 
-    return ResponseEntity.ok(productService.updateProduct(id, request));
-}
 }
