82321542d30d34591455f78473f5afd415959382
Fernando Villalobos <fe.villalobosv@duocuc.cl>
2026-05-20 15:46:45 -0400

    style: reorder imports

--- src/main/java/com/duoc/product/dto/request/ProductRequestDto.java
+++ src/main/java/com/duoc/product/dto/request/ProductRequestDto.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
